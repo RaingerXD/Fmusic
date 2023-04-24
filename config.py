00613b6f6fc10fa8a3c2d54277a2ed8a56362dba
RaingerXD <youtrx5@gmail.com>
2023-04-24 09:21:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "27025466"))
+API_HASH = getenv("API_HASH", "750ca688d4a6e58826e48d321ae4d498")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "5615921474"))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/2e2a2cd8c9db45ae73de6.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/2e2a2cd8c9db45ae73de6.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", "BQGcYDoAcGtL3w338d7UAsvyomqY6EA1AYiaJDty-OkD9u-P2TAO0bIETYNN4H6kK3GtkdGQ01fTthaMqv19JwZ1aa2QXTkHark5mqITCODNx9yBlq0orVe-kCwOggn-uBNrmpJueTCYdYvXeCGAygSSQ2cLwd-wIezP9fPUs_oTS-e7bQQvk9hsnqf9zgitnhYm1Jt664qp4aprCcbCxEP8x6S5wC0h8Ms7j8jleCwehQzfHmlfBAbUIewDDY3oJ9rXbChXWvq4cHyD_lpv7bTaDz4ss39WwquDdcz-0NtTa1c3Cq3QwaJet-bCHfpK88sFdcRm4dD9_qu6L2GGSAEQLDpgAAAABl18tsAA ")
 
 MUST_JOIN = getenv("MUST_JOIN", "raingerproject")
 
